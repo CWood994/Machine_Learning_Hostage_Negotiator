d291e4eb6bd30ad9db6ecec16c14a1d8e4f56846
Nate <borak.3@buckeyemail.osu.edu>
2017-02-08 14:38:05 -0500

    Basic gui added

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
+from kivy.graphics import *
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+from kivy.graphics.texture import *
+
+class hnsGame(Widget):
+	def watson_query(self, text):
+		print(text)
+	pass
+
+class hnsApp(App):
+	def build(self):
+		return hnsGame()
+		
+if __name__ == '__main__':
+    hnsApp().run()
