83b06867c8adb8a546c878a6a01d1672443ed0a5
Brandon Moore <moore.3071@osu.edu>
2017-02-24 11:27:26 -0500

    refactoring phase 1

---
+++ src/text_processor.py
@@ -0,0 +1,123 @@
+import json, sys, re, os
+
+from watson_developer_cloud import RetrieveAndRankV1
+from watson_developer_cloud import NaturalLanguageClassifierV1
+from os.path import join, dirname
+from watson_developer_cloud import TextToSpeechV1
+from subprocess import Popen
+from sys import platform
+
+class text_processor():
+
+    def __init__(self):
+        self.init_services()
+        #regular expression identifier for text that goes to R&R
+        self.rr_text_id = "^\w*\s*watson[,.\-!]{0,1}\s+"
+
+    def cleanse_rr_string(self, text):
+        return re.sub(self.rr_text_id,'',text, flags=re.IGNORECASE)
+
+    def init_services(self):
+        self.text_to_speech = TextToSpeechV1(
+            username='f112c56c-35ec-4728-b1ca-fe29fcd36f58',
+            password='vijGcGcSV2KX',
+            x_watson_learning_opt_out=True)  # Optional flag
+
+        self.retrieve_and_rank = RetrieveAndRankV1(
+            username='6cb8a2a2-01fc-4bad-ae24-b741082df113',
+            password='cxSIPwhVnZft')
+        self.solr_cluster_id = 'scff9b5a52_0a69_4a59_8e98_85c2184af3c5'
+
+        self.natural_language_classifier = NaturalLanguageClassifierV1(
+            username='9215e28a-3cff-4d3b-ad99-44d35e641876',
+            password='6CXwnXGbblMh')
+
+        self.configs = self.retrieve_and_rank.list_configs(solr_cluster_id=self.solr_cluster_id)
+
+    def nlc_classify(self, text):
+        classes = self.natural_language_classifier.classify('f5bbc5x176-nlc-3978', text)
+        return classes
+
+    def nlc_classify_top_result(self, text):
+        return self.nlc_classify(text)['classes'][0]['class_name']
+
+    def rr_process(self, text):
+        answer = self.rr_query_first_result(text)
+        self.printlog(text)
+        #print newText in the Gui
+
+        try:
+            with open(join(dirname(__file__), 'output.wav'), 'wrb') as audio_file:
+                output = self.text_to_speech.synthesize(answer, accept='audio/wav', voice="en-US_MichaelVoice")
+                audio_file.write(output)
+        #Ignore audio problems if they exist instead of
+        #interrupting the user
+        except:
+            pass
+        return answer
+
+    def printlog(self, message):
+       f = open('/home/bam/Documents/code/CSE5914/src/log.txt','a') 
+       f.write(message+"\n")
+       f.close()
+
+    #Play an audio file
+    def play_wav(self, filename):
+        try:
+            with open(join(dirname(__file__), filename), 'rb') as audio_file:
+                if platform == "linux" or platform == "linux2" or platform == "darwin":
+                    Popen(["play", 'output.wav'])
+                elif platform == "win32":
+                    Popen(["sox", 'output.wav', '-t', 'waveaudio'], shell = True)
+        except:
+            pass
+
+    #Read in the user input and feed to R&R if Watson
+    #is mentioned, otherwise feed to the NLC
+    def user_input(self, text):
+        text = text.lower()
+        if self.isWatsonQuery(text):
+            self.printlog(text)
+            self.rr_process(re.sub(self.rr_text_id,'',text, flags=re.IGNORECASE))
+        else:
+            self.printlog("How?")
+            self.hostage_taker_query(text)
+
+    def isWatsonQuery(self, text):
+        if re.search(self.rr_text_id, text, re.IGNORECASE):
+            return True
+        else:
+            return False
+
+    def rr_query(self, text):
+        results = ''
+        if len(self.configs['solr_configs']) > 0:
+            collections = self.retrieve_and_rank.list_collections(
+                solr_cluster_id=self.solr_cluster_id)
+
+            pysolr_client = self.retrieve_and_rank.get_pysolr_client(self.solr_cluster_id,
+                                                                collections[
+                                                                    'collections'][0])
+            results = pysolr_client.search(text)
+        #Error catch for improper solr setup
+        else:
+            print("Error: Solr misconfigured. Configs couldn't be read.")
+            sys.exit(1)
+        return results
+
+    #sends a request(text) to IBM's servers for R&R and
+    #returns the response
+    def rr_query_first_result(self, text):
+        response = ''
+        results = self.rr_query(text)
+
+        i = 0
+        newText = ''
+
+        #This catches an empty docset which would
+        #otherwise throw errors
+        try:
+            response = results.docs[0]['body'][0]
+        except IndexError:
+            response = "I'm sorry, they don't teach that at the academy"
+        return response
